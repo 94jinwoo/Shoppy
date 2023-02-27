5f485eb5bc761e6ecadabd0aefde559ba773e571
94jinwoo <zuz1003@gmail.com>
2023-02-28 01:49:36 +0900

    Feat: firebase를 이용한 구글 로그인, 로그아웃 기능 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FcShop } from 'react-icons/fc';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import { useState, useEffect } from 'react';
+import User from './User';
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -13,7 +22,9 @@ export default function Header() {
         <Link to='/products'>Product</Link>
         <Link to='/carts'>Cart</Link>
         <Link to='/products/new'>New</Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logut</button>}
       </nav>
     </header>
   );
