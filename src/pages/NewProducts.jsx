68afef5ded4ca5848bde2e75385229cb3a05c125
94jinwoo <zuz1003@gmail.com>
2023-03-04 16:00:04 +0900

    Refactor: useQuery 사용하는 부분을 커스텀 훅으로 리팩토링

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { addNewProduct } from '../api/firebase';
 import { uploadImage } from '../api/uploader';
 import Button from '../components/ui/Button';
+import useProducts from '../hooks/useProducts';
 
-export default function NewProducts() {
+export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
-  const [isUploading, setIsUploading] = useState();
+  const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -22,12 +23,17 @@ export default function NewProducts() {
     setIsUploading(true);
     uploadImage(file)
       .then((url) => {
-        addNewProduct(product, url).then(() => {
-          setSuccess('성공적으로 제품이 추가되었습니다.');
-          setTimeout(() => {
-            setSuccess(null);
-          }, 4000);
-        });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess('성공적으로 제품이 추가되었습니다.');
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          }
+        );
       })
       .finally(() => setIsUploading(false));
   };
@@ -87,7 +93,7 @@ export default function NewProducts() {
           type='text'
           name='options'
           value={product.options ?? ''}
-          placeholder='옵션들 콤마(,)로 구분'
+          placeholder='옵션들(콤마(,)로 구분)'
           required
           onChange={handleChange}
         />
